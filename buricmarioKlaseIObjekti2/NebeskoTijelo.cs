b6476098c01b167c3bcc331b43bd8029513d0310
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-04-10 17:57:10 +0200

    Add files via upload

---
+++ buricmarioKlaseIObjekti2/NebeskoTijelo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmarioKlaseIObjekti2
+{
+    class NebeskoTijelo
+
+        {
+            double brzinaOkoOsi;
+            double brzinaOkoSunca;
+
+            public double BrzinaOkoOsi { get => brzinaOkoOsi; set => brzinaOkoOsi = value; }
+            public double BrzinaOkoSunca { get => brzinaOkoSunca; set => brzinaOkoSunca = value; }
+        }
+    }
+
---
+++ buricmarioKlaseIObjekti2/Planet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmarioKlaseIObjekti2
+{
+    class Planet
+    { 
+            public Planet(double brzinaOkoOsi, double brzinaOkoSunca)
+            {
+                brzinaOkoOsi = brzinaOkoOsi;
+                brzinaOkoSunca = brzinaOkoSunca;
+            }
+        }
+    }
+
